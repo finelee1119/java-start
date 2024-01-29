913a3bcb413c9fada525919b43b57ea077229375
finelee1119 <finelee1119@gmail.com>
2024-01-29 18:21:37 +0900

    java start

--- src/scanner/ScannerWhileEx3.java
+++ src/scanner/ScannerWhileEx3.java
@@ -4,19 +4,31 @@ import java.util.Scanner;
 
 public class ScannerWhileEx3 {
     public static void main(String[] args) {
+        // 여러 개의 숫자를 입력 받고
+        // 그 숫자들의 합계와 평균(소수점 아래까지)을 계산하기
+        // 마지막에는 -1을 입력하여 종료
+
         Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+        int input = 0;
 
         System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
 
         while (true) {
-            int num = scanner.nextInt();
-            scanner.nextLine();
+            input = scanner.nextInt(); // 숫자 입력 받기
 
-            if (num == -1) {
-                System.out.println("입력한 숫자들의 합계: " + sum);
-                System.out.println("입력한 숫자들의 평균: " + average);
+            if (input == -1) {
                 break;
             }
+
+            sum += input;
+            count++;
         }
+
+        double average = (double)sum / count;
+        System.out.println("입력한 숫자들의 합계: " + sum);
+        System.out.println("입력한 숫자들의 평균: " + average);
     }
 }
