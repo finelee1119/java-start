619da904ed46b353da337a97c761b7c7be4dfea9
finelee1119 <finelee1119@gmail.com>
2024-01-29 18:28:13 +0900

    java start

--- src/scanner/ScannerWhileEx4.java
+++ src/scanner/ScannerWhileEx4.java
@@ -8,11 +8,11 @@ public class ScannerWhileEx4 {
         int totalCost = 0;
 
         while(true) {
-            System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료");
-            int option = scanner.nextInt();
+            System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료"); // 숫자 + \n 입력
+            int option = scanner.nextInt(); // 숫자만 챙겨감
 
             if (option == 1) {
-                scanner.nextLine(); // 이걸 써줘야 결론에 상품명이 잘 들어감
+                scanner.nextLine(); // \n 없애주기 : 이걸 써줘야 결론에 상품명이 잘 들어감
 
                 System.out.print("상품명을 입력하세요: ");
                 String product = scanner.nextLine();
@@ -23,8 +23,8 @@ public class ScannerWhileEx4 {
                 System.out.print("구매 수량을 입력하세요: ");
                 int quantity = scanner.nextInt();
 
-                totalCost = price * quantity;
-                System.out.println("상품명:" + product + " 가격:" + price + " 수량:" + quantity + " 합계:" + totalCost);
+                totalCost += price * quantity; // 상품을 계속 추가할 수 있으니 누적되도록 해야 함
+                System.out.println("상품명:" + product + " 가격:" + price + " 수량:" + quantity + " 합계:" + price*quantity);
 
             } else if (option == 2) {
                 System.out.println("총 비용:" + totalCost);
