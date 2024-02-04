7962df57020f2b0bd8fb6070fedf62b6042c6e3b
finelee1119 <finelee1119@gmail.com>
2024-02-04 11:31:10 +0900

    java start

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+
+        int score = 80;
+
+        boolean result = 80 <= score && score <= 100;
+        System.out.println(result);
+
+    }
+}
---
+++ src/operator/Operator4.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum3 = 2*2 + 3*3; // (2*2) + (3*3)과 같다.
+        int sum4 = (2*2) + (3*3);
+
+        System.out.println("sum3 = " + sum3); // 13
+        System.out.println("sum4 = " + sum4); // 13
+    }
+}
