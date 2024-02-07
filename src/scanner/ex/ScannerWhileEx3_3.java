82bb1f368330532cac6806cd0f5894daf33a9ab5
finelee1119 <finelee1119@gmail.com>
2024-02-07 19:07:29 +0900

    java start

---
+++ src/scanner/ex/ScannerWhileEx3_3.java
@@ -0,0 +1,12 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3_3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
